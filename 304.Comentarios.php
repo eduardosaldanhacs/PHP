fb3c9e8c4956e026ad3fe5571493696faba8955d
Eduardo Saldanha <102400077+eduardosaldanhacs@users.noreply.github.com>
2023-07-13 11:21:25 -0300

    Curso Desenvolvimento Web na UDEMY - Aula 304
    Nessa aula aprendemos a fazer comentários no PHP

---
+++ 304.Comentarios.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <!-- Comentário HTML --> 
+    <?php
+        // Comentário de uma linha
+        echo "Utilizando a tag padrão";
+    
+
+         #Comentário de uma linha
+        echo print("Utilizando a tag impressão") 
+
+        /*Comentário
+            de multiplas
+                    linhas
+        */      
+    
+    ?>
+</body>
+</html>
