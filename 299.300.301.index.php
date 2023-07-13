52891e63c75251044f0723ad7853c2374852b545
Eduardo Saldanha <102400077+eduardosaldanhacs@users.noreply.github.com>
2023-07-13 11:19:07 -0300

    Curso Desenvolvimento Web na UDEMY - Aula 299.300.301

---
+++ 299.300.301.index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <?php
+        echo "Utilizando a tag padrão";
+    ?>
+
+    <br />
+
+    <?= "Utilizando a tag impressão" ?>
+    
+    <br />
+    
+    <? echo "Utilizando a tag curta" ?>
+</body>
+</html>
